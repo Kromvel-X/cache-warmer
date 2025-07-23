7cdddd29fa481d8e2ca726aa1521035e106901fe
Kromvel-X <kromveln@gmail.com>
2025-07-23 17:33:44 +0300

    Initial commit: Cache Warmer Telegram bot

---
+++ bot.js
@@ -0,0 +1,201 @@
+/**
+ * A Telegram bot that allows users to run a cache warm-up process.
+ * It fetches URLs from a sitemap index, warms them up using Puppeteer,
+ * and logs any errors encountered.
+ * The bot responds to the /run_cache_warm command and provides feedback on the process.
+ */
+
+import { Telegraf } from 'telegraf'
+import { Input } from 'telegraf';
+import { Markup } from 'telegraf';
+import { message } from 'telegraf/filters'
+import { runCacheWarm } from './cache-warm.js';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);// Replace with your bot token from BotFather
+const adminChatId = process.env.ADMIN_CHAT_ID; // Replace with your admin chat ID
+
+let isRunning = false;// Flag to check if the warm-up is already running
+let lastRunTimestamp = 0;
+
+/**
+ * Handles the /start command.
+ * It sends a welcome message to the user and provides a button to run the cache warm-up.
+ * @param {Object} ctx - The context object provided by Telegraf.
+ * @returns {void}
+*/
+bot.start(
+  async (ctx) => {
+    await ctx.reply(
+      "Welcome to the Cache Warm-up Bot! \nUse the command /run_cache_warm to start warming up the cache.",
+      Markup.keyboard(["/run_cache_warm"]).oneTime().resize(),
+      );
+  }
+);
+
+/**
+ * Handles the /run_cache_warm command.
+ * It initiates the cache warm-up process and sends a message to the user with the results.
+ * If the warm-up is already running, it informs the user to wait.
+ * @param {Object} ctx - The context object provided by Telegraf.
+ * @returns {Promise<void>}
+*/
+bot.command('run_cache_warm', async (ctx) => {
+  
+    const now = Date.now();
+    if (isRunning || now - lastRunTimestamp < 5000) { // защита от повторов в 5 секунд
+        return await ctx.reply('⚠️  The warm-up is already underway. \nPlease wait for it to finish.');
+    }
+    isRunning = true;
+    lastRunTimestamp = now;
+    
+    console.log(`[BOT] isRunning set to TRUE by user ${ctx.from.id}`);
+
+    const userID = ctx.message.from.id;
+    var message = await ctx.reply("🚀 Cache warm-up running \n⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜ 0%");
+    let lastProgressText = ""; 
+
+    /**
+     * 
+     * Handles the progress of the cache warm-up.
+     * It updates the progress bar and sends the updated message to the user.
+     * @param {number} processed - The number of URLs processed.
+     * @param {number} total - The total number of URLs to process.
+     * @returns {Promise<void>}
+     */
+    const onProgress = async (processed, total) => {
+      const percent = Math.round((processed / total) * 100);
+      const bars = Math.round(percent / 10);
+      const progressBar = "🟩".repeat(bars) + "⬜".repeat(10 - bars);
+
+      var newText = `🚀 Cache warm-up running \n ${progressBar} ${percent}%`;
+
+      if (newText === lastProgressText) return;
+      lastProgressText = newText;
+
+      try {
+        await ctx.telegram.editMessageText(
+          message.chat.id,
+          message.message_id,
+          undefined,
+          newText
+        );
+      } catch (e) {
+        console.error(`❌ Error updating progress: ${e.message}`);
+      }
+    };
+
+    await ctx.telegram.sendMessage(adminChatId, `User ${userID} ran a cache warmup.`);
+
+    (async () => {
+        try {
+          const { totalUrls, successCount, totalWorkTime, errors } = await runCacheWarm(10, true, onProgress);
+
+          let errorMessage = '';
+          if (errors.length > 10) {
+              errorMessage = `⚠️ Found ${errors.length} errors. The full list is written to the sent log file.`;
+              const logPath = saveErrorsToLog(errors);
+              await ctx.replyWithDocument(Input.fromLocalFile(logPath));
+              await ctx.telegram.sendDocument(adminChatId, Input.fromLocalFile(logPath));
+          } else if (errors.length) {
+              errorMessage = `❗ Errors:\n${errors.join('\n')}`;
+              const logPath = saveErrorsToLog(errors);
+              await ctx.telegram.sendDocument(adminChatId, Input.fromLocalFile(logPath));
+          } else {
+            errorMessage= '✅ No errors';
+          }
+
+          const message =
+              `✅ Warm-up complete!\n` +
+              `🔗 Total URLs: ${totalUrls}\n` +
+              `✅ Successfully “warmed up” URLs: ${successCount}\n` +
+              `🕒 Time spent: ${totalWorkTime} min\n` +
+              `${errorMessage}`;
+
+          await ctx.reply(message);
+          await ctx.telegram.sendMessage(adminChatId, message + `,\n user: ${userID}`);
+      } catch (e) {
+          await ctx.reply(`❌ Error: ${e.message}`);
+          await ctx.telegram.sendMessage(adminChatId, `❌ Error: ${e.message}, user: ${userID}`);
+      } finally {
+          isRunning = false;
+          console.log(`[BOT] isRunning reset to FALSE`);
+      }
+    })();
+});
+
+/**
+ * Handles incoming text messages.
+ * It sends a message to the user with instructions on how to use the bot.
+ * @param {Object} ctx - The context object provided by Telegraf.
+ * @returns {Promise<void>}
+ */
+bot.on(message('text'), async (ctx) => {
+  await ctx.reply(
+		"To work with the bot, use the command /run_cache_warm",
+		Markup.keyboard(["/run_cache_warm"]).oneTime().resize(),
+    );
+});
+
+/**
+ * Handles errors that occur during the bot's operation.
+ * It logs the error and sends a message to the user and admin.
+ * @param {Error} err - The error object.
+ * @param {Object} ctx - The context object provided by Telegraf.
+ * @returns {void}
+ */
+bot.catch((err, ctx) => {
+  console.error(`❌ Telegraf error for update type ${ctx.updateType}`, err);
+  ctx.reply(`⚠️ Internal error occurred: ${err.message}`).catch(() => {});
+  
+  // Sending to the administrator
+  ctx.telegram.sendMessage(
+    adminChatId,
+    `❌ Bot crashed on update type ${ctx.updateType}\nError: ${err.message}`
+  ).catch(() => {});
+});
+
+/** Sets up the webhook for the bot.
+ * Comment this out if you want to run the bot without a webhook.
+ * The bot will listen for incoming updates at the specified domain and path.
+ * The port is set to 3000, which should match your server configuration.
+ * @returns {Promise<void>}
+ *
+*/
+bot.launch({
+  webhook: {
+    domain: process.env.DOMAIN, // Replace with your domain
+    path: '/webhook',
+    port: 3000
+  }
+}).then(() => {
+  console.log('🤖 The bot is running and listening for commands...');
+}).catch((err) => {
+  console.error('❌ Launch error:', err);
+});
+
+
+// Uncomment the line below to run the bot without webhook
+// bot.launch();
+
+/**
+ * Saves errors to a log file.
+ * @param {Array} errors - The array of error messages to save.
+ * @returns {string} The path to the log file.
+*/
+function saveErrorsToLog(errors) {
+  const logDir = '/root/cachewarmer/logs';
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+  }
+
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+  const filePath = `${logDir}/warm-errors-${timestamp}.log`;
+
+  fs.writeFileSync(filePath, errors.join('\n'), 'utf-8');
+  return filePath;
+}
---
+++ cache-warm.js
@@ -0,0 +1,252 @@
+
+/**
+ * A simple cache warming utility that fetches URLs from a sitemap index,
+ * warms them up using Puppeteer, and logs any errors encountered.
+*/
+
+import axios from 'axios';
+import { XMLParser } from 'fast-xml-parser';
+import puppeteer from 'puppeteer-core';
+import { performance } from 'perf_hooks';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const parser = new XMLParser(); // XML parser instance
+
+/**
+ * Runs the cache warm-up process.
+ * @param {number} concurrency - The number of concurrent requests.
+ * @param {boolean} deepWarm - Whether to perform a deep warm-up (loading all resources).
+ * @param {Function} onProgress - A callback function to handle progress updates.
+ * @returns {Promise<Object>} An object containing the total URLs, success count, total work time, and any errors encountered.
+*/
+export async function runCacheWarm(concurrency = 5, deepWarm = true, onProgress) {
+  const {urls, errors: sitemapErrors} = await collectAllUrls(process.env.SITE_MAP_URL);
+  const {successCount, totalWorkTime, errors: warmErrors} =  await warmUrls(urls, concurrency, deepWarm, onProgress);
+  return {
+     totalUrls: urls.length, 
+     successCount, 
+     totalWorkTime, 
+     errors: [...sitemapErrors, ...warmErrors]
+  }
+};
+
+/**
+ * Collects all URLs from the sitemap index. 
+ * @param {string} indexUrl - The URL of the sitemap index.
+ * @returns {Promise<Object>} An object containing an array of URLs and any errors encountered.
+*/
+async function collectAllUrls(indexUrl) {
+  const indexXml = await getXml(indexUrl);
+  const sitemapUrls = parseSitemapIndex(indexXml);
+  let errors = [];
+  let allUrls = [];
+  for (const sitemapUrl of sitemapUrls) {
+    try {
+      const sitemapXml = await getXml(sitemapUrl);
+      const urls = parseUrlSet(sitemapXml);
+      allUrls.push(...urls);
+    } catch (e) {
+      errors.push(`❌ Load error ${sitemapUrl}: ${e.message}`);
+    }
+  }
+
+  return {
+    urls: allUrls,
+    errors
+  };
+}
+
+/** 
+ * Warming up URLs with Puppeteer.
+ * @param {Array} urls - The URLs to warm up.
+ * @param {number} concurrency - The number of concurrent requests.
+ * @param {boolean} deepWarm - Whether to perform a deep warm-up (loading all resources).
+ * @param {Function} onProgress - A callback function to handle progress updates.
+ * @returns {Promise<Object>} An object containing the success count, total work time, and any errors encountered.
+*/
+async function warmUrls(urls, concurrency, deepWarm, onProgress) {
+  // console.log(`Starting warm-up for ${urls.length} URLs with concurrency ${concurrency} and deepWarm=${deepWarm}`);
+  const start = getCurrentTime();
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      executablePath: '/usr/bin/chromium-browser',
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+  } catch (e) {
+    throw new Error(`❌ Critical error: failed to launch the browser: ${e.message}`);
+  }
+
+  const chunks = chunkArray(urls, concurrency);
+  const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36';
+  var successCount = 0;
+  let errors = [];
+  let processed = 0;
+  let total = urls.length;
+  
+  for (const batch of chunks) {
+    // console.log(successCount);
+  
+    // Uncomment the following lines to stop after warming up 1 (concurrency)  URLs
+    // if(successCount>=20){
+    //   break;
+    // }
+    await Promise.all(batch.map(async (url) => {
+      const page = await browser.newPage();
+      try {
+        await page.setUserAgent(userAgent);
+        await page.setExtraHTTPHeaders({
+          'Accept-Language': 'en-US,en;q=0.9',
+        });
+        // console.log(`Visiting: ${url}`);
+
+        if (deepWarm) {
+          await page.setRequestInterception(false);
+        } else {
+          await page.setRequestInterception(true);
+          page.on('request', (req) => {
+            if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+              req.abort();
+            } else {
+              req.continue();
+            }
+          });
+        }
+          
+        const response = await page.goto(url, { waitUntil: 'networkidle2', timeout: 30000 });
+        if (!response) {
+          errors.push(`⚠️ No response from the server at ${url}`);
+          return;
+        }
+
+        let headers = {};
+        let status = 0;
+        try {
+          headers = response.headers();
+          status = response.status();
+        } catch (e) {
+          errors.push(`⚠️ Error when extracting headers on ${url}: ${e.message}`);
+          return;
+        }
+
+        try {
+          await page.waitForSelector('body', { timeout: 5000 });
+        } catch (e) {
+          errors.push(`⚠️ Page ${url} failed to load <body> in 5 secs`);
+          return;
+        }
+
+        const html = await page.content();
+        const isHtmlClosed = /\<\/html\>\s*$/i.test(html);
+        if (!isHtmlClosed) {;
+          errors.push(`⚠️ The ${url} page is not fully loaded (no </html>)`);
+          return;
+        }
+          
+        // console.log(`Response Status: ${status}`);
+        // console.log('✅ Cache headers:', {
+        //   url,
+        //   'x-cache': headers['x-cache'],
+        //   'cf-cache-status': headers['cf-cache-status'],
+        //   'x-cacheable': headers['x-cacheable'],
+        //   'cache-control': headers['cache-control']
+        // });
+
+        if (status === 200 && isHtmlClosed) {
+          successCount++;
+
+          processed++;
+          if (onProgress && processed % concurrency === 0 || processed === total) {
+            // console.log(`вызов onProgress: ${processed}/${total}`);
+            await onProgress(processed, total); // ✅ вызываем прогресс
+          }
+          
+          // console.log(`✅ Successfully warmed up: ${successCount}`);
+        }
+      } catch (e) {
+        errors.push(`❌ Error on ${url}: ${e.message}`);
+      } finally {
+        if (!page.isClosed()) {
+          await page.close(); 
+        }
+      }
+    }));
+    await sleep(500); // пауза между чанками
+  }
+
+  try {
+    await browser.close();
+  } catch (e) {
+    throw new Error(`❌ Critical error: failed to close the browser: ${e.message}`);
+  }
+
+  const end = getCurrentTime();
+  const totalWorkTime = ((end - start) / 1000 / 60).toFixed(2);
+
+  return {
+    successCount,
+    totalWorkTime,
+    errors
+  };
+}
+
+/** Fetches and parses XML from a given URL.
+ * @param {string} url - The URL to fetch the XML from.
+ * @returns {Promise<Object>} The parsed XML object.
+*/
+async function getXml(url) {
+  const res = await axios.get(url);
+  return parser.parse(res.data);
+}
+
+/** Parses the sitemap index XML to extract URLs.
+ * @param {Object} xml - The parsed XML object.
+ * @returns {Array} An array of sitemap URLs.
+*/
+function parseSitemapIndex(xml) {
+  return xml.sitemapindex?.sitemap?.map(s => s.loc) || [];
+}
+
+/** * Parses the URL set from a sitemap XML.
+ * @param {Object} xml - The parsed XML object.
+ * @returns {Array} An array of URLs.
+*/
+function parseUrlSet(xml) {
+  const urlset = xml.urlset?.url;
+  if (!urlset) return [];
+  const urls = Array.isArray(urlset) ? urlset : [urlset];
+  return urls.map(item => item.loc);
+}
+
+/** * Returns the current time in milliseconds.
+ * @returns {number} The current time in milliseconds.
+*/
+function getCurrentTime(){
+  return performance.now();
+}
+
+/** * Splits an array into chunks of a specified size.
+ * @param {Array} arr - The array to split.
+ * @param {number} size - The size of each chunk.
+ * @returns {Array} An array of chunks.
+*/
+function chunkArray(arr, size) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
+/**
+ * Pauses execution for a specified number of milliseconds.
+ * @param {number} ms - The number of milliseconds to pause.
+ * @returns {Promise<void>} A promise that resolves after the specified time.
+*/
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
